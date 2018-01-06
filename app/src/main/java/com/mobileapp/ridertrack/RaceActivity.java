99a7c085458f21560fd94a9c77393f7d94464a19
Giulia Leonardi <giulia.leonardi@mail.polimi.it>
2018-01-06 14:01:50 +0100

    Permission checked. The application ask the user permissions to activate GPS

--- app/src/main/java/com/mobileapp/ridertrack/RaceActivity.java
+++ app/src/main/java/com/mobileapp/ridertrack/RaceActivity.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.location.Location;
@@ -11,6 +12,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
+import android.provider.Settings;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -29,6 +31,16 @@ import com.facebook.share.model.ShareOpenGraphContent;
 import com.facebook.share.model.ShareOpenGraphObject;
 import com.facebook.share.model.SharePhotoContent;
 import com.facebook.share.widget.ShareDialog;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.BufferedReader;
@@ -98,6 +110,7 @@ public class RaceActivity extends AppCompatActivity {
          * Preventing the screen sleep
          */
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        displayLocationSettingsRequest(this);
         /*
          * Binding the activity to its layout and creating an array list of "Location" objects
          */
@@ -291,6 +304,10 @@ public class RaceActivity extends AppCompatActivity {
      * StopTracking method closes the RaceActivity and redirects the user back to EventsListActivity
      */
     private void stopTracking(){
+        SharedPreferences sp = getSharedPreferences("ActualStartingTime", MODE_PRIVATE);
+        SharedPreferences.Editor Ed = sp.edit();
+        Ed.putString("ast", "");
+        Ed.commit();
         stopService(locationService);
         stopChronometer();
         /*Intent eventsList = new Intent(this, EventsListActivity.class);
@@ -308,8 +325,7 @@ public class RaceActivity extends AppCompatActivity {
         String ast=sp.getString("ast", "");
         if(ast.equals("")) {
             actualStartingTime = SystemClock.elapsedRealtime();
-            SharedPreferences sp2 = getSharedPreferences("ActualStartingTime", MODE_PRIVATE);
-            SharedPreferences.Editor Ed = sp2.edit();
+            SharedPreferences.Editor Ed = sp.edit();
             Ed.putString("ast", String.valueOf(actualStartingTime));
             Ed.commit();
         }else{
@@ -572,12 +588,52 @@ public class RaceActivity extends AppCompatActivity {
             }
         }
     }
-    public void logLargeString(String str) {
-        if(str.length() > 3000) {
-            Log.i(TAG, str.substring(0, 3000));
-            logLargeString(str.substring(3000));
-        } else {
-            Log.i(TAG, str); // continuation
-        }
+
+    /**
+     * DisplayLocationSettingsRequest method shows to user a pop up requiring the permission to activate GPS of the
+     * mobile phone.
+     * @param context: context in which method is called
+     */
+    private void displayLocationSettingsRequest(Context context) {
+        GoogleApiClient googleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(LocationServices.API).build();
+        googleApiClient.connect();
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(10000 / 2);
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        Log.i(TAG, "All location settings are satisfied.");
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        Log.i(TAG, "Location settings are not satisfied. Show the user a dialog to upgrade location settings ");
+
+                        try {
+                            /*
+                             * Show the dialog by calling startResolutionForResult(), and check the result
+                             * in onActivityResult().
+                             */
+                            status.startResolutionForResult(RaceActivity.this, 1000);
+                        } catch (IntentSender.SendIntentException e) {
+                            Log.i(TAG, "PendingIntent unable to execute request.");
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        Log.i(TAG, "Location settings are inadequate, and cannot be fixed here. Dialog not created.");
+                        break;
+                }
+            }
+        });
     }
 }
